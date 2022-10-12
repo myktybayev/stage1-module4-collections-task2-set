cc83efcfd691c94d45edb283de5c392f7551bc53
Bakhytzhan <mykty-baha@bk.ru>
2022-10-12 18:26:48 +0600

    task2

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -5,5 +5,16 @@ import java.util.Set;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        Set<String> res = new HashSet<>();
+        thirdSet.removeAll(firstSet);
+        thirdSet.removeAll(secondSet);
+
+        for (String w: firstSet) {
+            if(secondSet.contains(w)){
+                res.add(w);
+            }
+        }
+        res.addAll(thirdSet);
+        return res;
     }
 }
