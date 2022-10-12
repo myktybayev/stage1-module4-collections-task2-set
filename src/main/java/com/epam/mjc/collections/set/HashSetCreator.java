0f061d3c63f0baceb32ac0541ffee5798f5d3fd6
Bakhytzhan <mykty-baha@bk.ru>
2022-10-12 18:26:16 +0600

    task1

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,21 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (Integer n: sourceList) {
+            if(n%2==0) {
+                while (n % 2 == 0) {
+                    set.add(n);
+                    n /= 2;
+                }
+                set.add(n);
+            }else{
+                set.add(n);
+                set.add(n*2);
+            }
+        }
+
+        return set;
     }
 }
